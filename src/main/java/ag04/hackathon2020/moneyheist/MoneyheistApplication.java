ff08bb30807e1f482a6b62664ee4ed83f155a46b
brunotot <brunotot10000@gmail.com>
2020-12-24 23:29:05 +0100

    Create Spring Boot app

---
+++ src/main/java/ag04/hackathon2020/moneyheist/MoneyheistApplication.java
@@ -0,0 +1,13 @@
+package ag04.hackathon2020.moneyheist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyheistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyheistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ag04/hackathon2020/moneyheist/MoneyheistApplicationTests.java
@@ -0,0 +1,13 @@
+package ag04.hackathon2020.moneyheist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyheistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
