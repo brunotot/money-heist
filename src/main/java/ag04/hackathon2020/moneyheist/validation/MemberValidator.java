98c86b6061ebb3ee8f5de16c4e0f89944219fa04
brunotot <brunotot10000@gmail.com>
2020-12-25 14:41:41 +0100

    Refactor MemberValidator to separate each validation

--- src/main/java/ag04/hackathon2020/moneyheist/validation/MemberValidator.java
+++ src/main/java/ag04/hackathon2020/moneyheist/validation/MemberValidator.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import ag04.hackathon2020.moneyheist.entity.Member;
 import ag04.hackathon2020.moneyheist.entity.MemberSkill;
+import ag04.hackathon2020.moneyheist.entity.Skill;
 import ag04.hackathon2020.moneyheist.exception.ApiException;
 import ag04.hackathon2020.moneyheist.mapper.MemberMapper;
 
@@ -21,7 +22,7 @@ public class MemberValidator {
 		this.memberMapper = memberMapper;
 	}
 	
-	public void validate(Member member) {
+	public void validateDuplicateSkillNames(Member member) {
 		List<MemberSkill> skills = member.getMemberSkills();
 		int totalCount = skills.size();
 		if (skills != null && !skills.isEmpty()) {
@@ -29,31 +30,41 @@ public class MemberValidator {
 			if (distinctCount != totalCount) {
 				throw new ApiException(HttpStatus.BAD_REQUEST, "Duplicate member skill names", "Duplicate member skill names were provided. Please, make sure to only provide distinct skill names", null);
 			}
-			if (skills != null) {
-				for (MemberSkill ms : skills) {
-					ms.getSkill().setName(ms.getSkill().getName());
-					String level = ms.getLevel();
-					if (level != null && !"".equals(level)) {
-						int asteriskCount = (int) level.chars().filter(ch -> ch == '*').count();
-						if (level.length() > 10 || asteriskCount != level.length()) {
-							throw new ApiException(HttpStatus.BAD_REQUEST, "Invalid skill level provided", "Invalid skill level was provided. Please, make sure to only provide a string consisted of 0 to 10 asterisks (if left empty, 1 asterisk gets applied as a default value)", null);
-						}
+		}
+	}
+	
+	public void validateSkillLevels(Member member) {
+		List<MemberSkill> skills = member.getMemberSkills();
+		if (skills != null && !skills.isEmpty()) {
+			for (MemberSkill ms : skills) {
+				ms.getSkill().setName(ms.getSkill().getName());
+				String level = ms.getLevel();
+				if (level != null && !"".equals(level)) {
+					int asteriskCount = (int) level.chars().filter(ch -> ch == '*').count();
+					if (level.length() > 10 || asteriskCount != level.length()) {
+						throw new ApiException(HttpStatus.BAD_REQUEST, "Invalid skill level provided", "Invalid skill level was provided. Please, make sure to only provide a string consisted of 0 to 10 asterisks (if left empty, 1 asterisk gets applied as a default value)", null);
 					}
 				}
 			}
 		}
-		
+	}
+	
+	public void validateMemberAlreadyExists(Member member) {
 		String memberEmail = member.getEmail();
 		Member existingMember = memberMapper.findByEmail(memberEmail);
 		if (existingMember != null) {
 			throw new ApiException(HttpStatus.BAD_REQUEST, "Member already exists", "Member with email '" + member.getEmail() + "' already exist. Please, make sure to provide a member with unique email", null);
 		}
-		
+
+	}
+	
+	public void validateMainSkillReference(Member member) {
+		List<Skill> skills = member.getSkills();
 		String mainSkill = member.getMainSkill() == null ? null : member.getMainSkill().getName();
 		if (member.getMainSkill() != null) {
 			member.getMainSkill().setName(mainSkill);
 		}
-		if (mainSkill != null && !mainSkill.isEmpty() && !skills.stream().map(ms -> ms.getSkill().getName()).anyMatch(s -> s.equals(mainSkill))) {
+		if (mainSkill != null && !mainSkill.isEmpty() && Skill.find(mainSkill, skills) == null) {
 			throw new ApiException(HttpStatus.BAD_REQUEST, "mainSkill doesn't reference any skill from skills array", "Invalid mainSkill was provided. Please, make sure to only provide mainSkill which references member's skills from skills array",  null);
 		}
 	}
